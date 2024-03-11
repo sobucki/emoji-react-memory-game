239df5b6557751423c3713e3e4a624c5a6b1ed0e
Rafael Soares Sobucki <rafaelsobucki.smartsoftware@bexsbanco.com.br>
2024-03-11 17:09:49 -0300

    Shuffle function and tests

--- src/util/__tests__/utils.spec.ts
+++ src/util/__tests__/utils.spec.ts
@@ -1,4 +1,4 @@
-import { duplicateUniqueList } from "../utils";
+import { duplicateUniqueList, shuffle } from "../utils";
 
 describe("util tests", () => {
   describe("duplicateUniqueList", () => {
@@ -21,6 +21,44 @@ describe("util tests", () => {
     it("should clean remove duplicated values", () => {
       const result = duplicateUniqueList(["a", "a"]);
       expect(result).toEqual(["a", "a"]);
+    });
+  });
+
+  describe("shuffle", () => {
+    it("should return a same size list", () => {
+      const list1 = ["a", "b", "c"];
+      const result1 = shuffle(list1);
+      expect(result1.length).toEqual(3);
+    });
+
+    it("should not modified the source list", () => {
+      const list1 = ["a", "b", "c", "d", "e", "f"];
+      const result1 = shuffle(list1);
+      expect(result1).not.toEqual(list1);
+    });
+
+    it("should contain the same elements as the original array", () => {
+      const array = [1, 2, 3, 4, 5];
+      const shuffledArray = shuffle(array);
+      expect(shuffledArray.sort()).toEqual(array.sort());
+    });
+
+    it("should shuffle the array elements", () => {
+      const array = [1, 2, 3, 4, 5];
+      const shuffledArray = shuffle(array);
+      expect(shuffledArray).not.toEqual(array);
+    });
+
+    it("should handle an empty array", () => {
+      const array: number[] = [];
+      const shuffledArray = shuffle(array);
+      expect(shuffledArray).toEqual([]);
+    });
+
+    it("should handle an array with one element", () => {
+      const array = [1];
+      const shuffledArray = shuffle(array);
+      expect(shuffledArray).toEqual([1]);
     });
   });
 });
--- src/util/utils.ts
+++ src/util/utils.ts
@@ -2,3 +2,12 @@ export const duplicateUniqueList = <T>(list: T[]): T[] => {
   const cleanList = [...new Set(list)];
   return [...cleanList, ...cleanList];
 };
+
+export const shuffle = <T>(array: T[]): T[] => {
+  const arrayCopy = [...array];
+  for (let i = arrayCopy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
+  }
+  return arrayCopy;
+};
